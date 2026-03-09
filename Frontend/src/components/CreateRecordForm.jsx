a2974515aaf1c5928dc9390070906f527955fc3e
Azrael <serarseniy07@gmail.com>
2026-03-09 20:06:20 +0300

    Added frontend

---
+++ Frontend/src/components/CreateRecordForm.jsx
@@ -0,0 +1,14 @@
+﻿import {Button, Input, Textarea} from "@chakra-ui/react";
+
+function CreateRecordForm() {
+    return (
+        <form className="w-full flex flex-col gap-3">
+            <h3 className="font-bold text-teal-400 text-xl">Создание ToDo</h3>
+            <Input placeholder={"Название"} colorPalette="teal" css={{borderColor: "#004D40"}} />
+            <Textarea placeholder={"Описание"} colorPalette="teal" css={{borderColor: "#004D40"}}/>
+            <Button colorPalette="teal" variant="outline">Создать</Button>
+        </form>
+    );
+}
+
+export default CreateRecordForm;
